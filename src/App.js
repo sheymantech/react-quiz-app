c668e4aa0c63cc25ca9f1fdd70535e4b77af7502
sheymantech <olaniyansheyi1704@gmail.com>
2024-03-17 06:14:40 +0100

    new feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import Header from "./Header";
 import Main from "./MainBody";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, statue: "error" };
+
+    default:
+      throw new Error("Action unknow");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
