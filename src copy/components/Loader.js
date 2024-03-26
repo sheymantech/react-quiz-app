dc838ca9a4acb7eb5d197a0d645873e478510b01
sheymantech <olaniyansheyi1704@gmail.com>
2024-03-26 21:32:01 +0100

    new feature

---
+++ src copy/components/Loader.js
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+      <p> please wait,Loading questions...</p>
+    </div>
+  );
+}
