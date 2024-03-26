8c14a4c651265c111c14bea3c85b43e2af7bd880
sheymantech <olaniyansheyi1704@gmail.com>
2024-03-26 21:56:37 +0100

    new featuer

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import Header from "./Header";
 import Main from "./MainBody";
 import Loader from "./Loader";
 import Error from "./Error";
-import { useEffect, useReducer } from "react";
 import StartScreen from "./StartScreen";
 import Questions from "./Questions";
 import NextButton from "./NextButton";
@@ -10,82 +9,19 @@ import Progress from "./Progress";
 import FinishedScreen from "./FinishedScreen";
 import Footer from "../Footer";
 import Timer from "./Timer";
+import { useQuiz } from "../context/QuizContext";
 
-const SECS_PER_QUESTION = 30;
-
-const initialState = {
-  questions: [],
-  status: "loading",
-  index: 0,
-  answer: null,
-  points: 0,
-  highscore: 0,
-  secondRemaining: null,
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "dataReceived":
-      return { ...state, questions: action.payload, status: "ready" };
-
-    case "dataFailed":
-      return { ...state, status: "error" };
-    case "start":
-      return {
-        ...state,
-        status: "active",
-        secondRemaining: state.questions.length * SECS_PER_QUESTION,
-      };
-    case "newAnswer":
-      const question = state.questions.at(state.index);
-      return {
-        ...state,
-        answer: action.payload,
-        points:
-          action.payload === question.correctOption
-            ? state.points + question.points
-            : state.points,
-      };
-    case "nextQuestion":
-      return { ...state, index: state.index + 1, answer: null };
-    case "finish":
-      return {
-        ...state,
-        status: "finished",
-        highscore:
-          state.points > state.highscore ? state.points : state.highscore,
-      };
-    case "restart":
-      return { ...state, status: "active", answer: null, index: 0, points: 0 };
-    case "tick":
-      return {
-        ...state,
-        secondRemaining: state.secondRemaining - 1,
-        status: state.secondRemaining === 0 ? "finished" : state.status,
-      };
-
-    default:
-      throw new Error("Action unknow");
-  }
-}
 export default function App() {
-  const [
-    { questions, status, index, answer, points, highscore, secondRemaining },
+  const {
+    questions,
+    status,
+    index,
+    answer,
+    points,
+    highscore,
+    secondRemaining,
     dispatch,
-  ] = useReducer(reducer, initialState);
-
-  const numQuestions = questions.length;
-  const maxPossiblePoints = questions.reduce(
-    (prev, cur) => prev + cur.points,
-    0
-  );
-
-  useEffect(function () {
-    fetch(`http://localhost:8000/questions`)
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => dispatch({ type: "dataFailed" }));
-  }, []);
+  } = useQuiz();
 
   return (
     <div className="app">
---
+++ src/context/QuizContext.jsx
@@ -0,0 +1,108 @@
+import { createContext, useContext } from "react";
+import { useEffect, useReducer } from "react";
+
+const QuizContext = createContext();
+
+const SECS_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return { ...state, status: "active", answer: null, index: 0, points: 0 };
+    case "tick":
+      return {
+        ...state,
+        secondRemaining: state.secondRemaining - 1,
+        status: state.secondRemaining === 0 ? "finished" : state.status,
+      };
+
+    default:
+      throw new Error("Action unknow");
+  }
+}
+
+function QuizProvider({ children }) {
+  const [
+    { questions, status, index, answer, points, highscore, secondRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <QuizContext.Provider
+      value={{
+        questions,
+        status,
+        index,
+        answer,
+        points,
+        highscore,
+        secondRemaining,
+        dispatch,
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+}
+
+function useQuiz() {
+  const context = useContext(QuizContext);
+  if (context === undefined) throw new Error("Unknown action");
+
+  return context;
+}
+
+export { QuizProvider, useQuiz };
